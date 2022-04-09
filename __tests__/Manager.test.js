9282b3dfd5c5dfb2707b40c4477478f2e11b52c9
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-08 19:24:27 -0700

    Update Manager.test.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,19 @@
-
 const Manager = require("../lib/Manager");
 
-test()
+test("Can get the office number through the constructor", () =>{
+    const testValue = 12;
+    const e = new Manager ("Mot",10,"Mot@gmail.com",testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+test("With getRole(), it should return Manager", () => {
+    const testRole = "Manager"
+    const e = new Manager ("Mot",10,"Mot@gmail.com",7);
+    expect(e.getRole()).toBe(testRole);
+});
+
+test("With getOffice(), it should return office number", () => {
+    const testValue = 39
+    const e = new Manager ("Mot",10,"Mot@gmail.com",39);
+    expect(e.getOffice()).toBe(testValue);
+    
+});
