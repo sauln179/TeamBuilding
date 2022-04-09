136dde0eb65c2e3a0e26cbbfd5c2673eba8dbb42
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-08 19:24:04 -0700

    Update Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const intern = require("../lib/Intern");
+
+test("Can get School through the constructor", () =>{
+    const testSchool = "SF State";
+    const e = new intern ("Maxie",45,"Maxie@gmail.com",testSchool);
+    expect(e.school).toBe(testSchool);
+});
+test("With getRole(), it should return intern", () => {
+    const testRole = "Intern"
+    const e = new intern ("Maxie",45,"Maxie@gmail.com","SF State");
+    expect(e.getRole()).toBe(testRole);
+});
+
+test("With getSchool(), it should return school", () => {
+    const testSchool = "UCLA"
+    const e = new intern ("Maxie",45,"Maxie@gmail.com","UCLA");
+    expect(e.getSchool()).toBe(testSchool);
+    
+});
