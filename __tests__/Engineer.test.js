b4aca910677f113228a7aec4ecfd2817f1df9c9d
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-08 19:23:21 -0700

    Update Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require("../lib/Engineer");
+
+test("Can get GitHUB account through the constructor", () =>{
+    const testGit = "GitTest";
+    const e = new Engineer ("Willie",223,"Willie@gmail.com",testGit);
+    expect(e.github).toBe(testGit);
+});
+test("With getRole(), it should return Engineer", () => {
+    const testRole = "Engineer"
+    const e = new Engineer ("Willie",230,"Willie@gmail.com","GitTest");
+    expect(e.getRole()).toBe(testRole);
+});
+
+test("With getGitHub(), it should return office number", () => {
+    const testGit = "GitTest"
+    const e = new Engineer ("Willie",230,"Willie@gmail.com","GitTest");
+    expect(e.getGithub()).toBe(testGit);
+    
+});
