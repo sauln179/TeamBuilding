904df55dff45cf4f78be87afa5e795ec325433c1
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-09 16:23:04 -0700

    Add comments.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+//Add all required files, and functions
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require('inquirer');
 const fs = require('fs');
+const render = require("./lib/Render");
+
+//Set an array to keep track of all user input.
 const employees = [];
+//Initialize function, where it will ask user prompt.
 init = () =>{
     console.log("Answer the Following Prompts")
     return inquirer.prompt([
@@ -15,30 +20,34 @@ init = () =>{
         {
             type: "input",
             message: "Enter Manager's ID",
-            name: "managerID"
+            name: "id"
         },
         {
             type: "input",
             message: "Enter Manager's email",
-            name: "managerEmail"
+            name: "email"
         },
         {
             type: "input",
             message: "Enter Manager's office number",
-            name: "managerON"
+            name: "officeNumber"
         }
 
-    ]).then((managerResults) => {
-        managerResults.role = "Manager";
-        const { name, id, email, officeNumber, role } = managerResults;
+    ])
+    //Take input for the manager, and saves it, then pushes into employees array.
+    .then((managerInput) => {
+        managerInput.role = "Manager";
+        const { name, id, email, officeNumber, role } = managerInput;
         const newManager = new Manager(name, id, email, officeNumber, role);
         employees.push(newManager);
-        // addEmployee();
-        console.log(managerResults);
+       //To make sure user input is indeed accurate.
+        console.log(managerInput);
+        //Ask user to add another employee.
         addEmployee();
     })
 };
-
+//Add employee. WIll ask yes or no question, depends on answer it will continue
+//or start making html file for user.
 addEmployee = () => {
     return inquirer.prompt([
         {
@@ -55,11 +64,11 @@ addEmployee = () => {
             employeeType();
         }
         else {
-            redernHTML();
+            renderHtml();
         }
     })
 }
-
+//Ask user which type of employee: Intern, and Engineer, then will run appropriate functions
 employeeType = () => {
     return inquirer.prompt({
         type: "list",
@@ -76,7 +85,7 @@ employeeType = () => {
         }
     })
 };
-
+//For adding engineer
 addEngineer = () => {
     return inquirer.prompt([
         //question about Engineer
@@ -100,17 +109,19 @@ addEngineer = () => {
             message: "Engineer's github user name:",
             name: "github"
         }
-    ]).then((engineerResults) => {
-        engineerResults.role = "Engineer";
-        const { name, id, email, github, role } = engineerResults;
+    ])
+    //Same as manager
+    .then((engineerInput) => {
+        engineerInput.role = "Engineer";
+        const { name, id, email, github, role } = engineerInput;
         const newEngineer = new Engineer(name, id, email, github, role);
         employees.push(newEngineer);
         //ask if user wants to add another team member
-        console.log(engineerResults);
+        console.log(engineerInput);
         addEmployee();
     });
 };
-
+//Ask for intern info
 addIntern = () => {
     return inquirer.prompt([
         //question about Intern
@@ -134,15 +145,29 @@ addIntern = () => {
             message: "Intern's school:",
             name: "school"
         }
-    ]).then((internResults) => {
-        internResults.role = "Intern";
-        const { name, id, email, school, role } = internResults;
+    ])
+    //Same as above.
+    .then((internInput) => {
+        internInput.role = "Intern";
+        const { name, id, email, school, role } = internInput;
         const newIntern = new Intern(name, id, email, school, role);
         employees.push(newIntern);
         //ask if user wants to add another team member
+        console.log(internInput);
         addEmployee();
-        console.log(internResults);
+        
     });
 };
-
+//Function to create a file called newTeam.html
+renderHtml = () => {
+    const buildHTML = render(employees);
+    fs.writeFile("newTeam.html", buildHTML, (err) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            return console.log("Team HTML file has been created!")
+        };
+    })
+};
+//Starts code.
 init();
